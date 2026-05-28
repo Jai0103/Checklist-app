80e2eb838788af5e98313ed42289c249a8d87c80
Jairus <orolazajairus@gmail.com>
2026-05-28 19:44:41 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
+export default function App() {
+  const [auth, setAuth] = useLocalStorage("auth", false);
+
+  return auth ? (
+    <Dashboard onLogout={() => setAuth(false)} />
+  ) : (
+    <Login onLogin={() => setAuth(true)} />
+  );
+}
